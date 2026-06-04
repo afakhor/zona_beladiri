a4495517f1894bc2c92056d24a2cce97662f749e
afakhor <31812946+afakhor@users.noreply.github.com>
2026-06-04 08:08:26 +0700

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,40 +1,37 @@
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("dev.flutter.flutter-gradle-plugin")
+    id "com.android.application"
+    id "kotlin-android"
+    id "dev.flutter.flutter-gradle-plugin"
 }
 
 android {
-    namespace = "com.heruwngchn.addhmescrn" // Sesuaikan dengan package name aplikasi Coach
-    compileSdk = flutter.compileSdkVersion
+    namespace "com.heruwngchn.addhmescrn" // Sesuaikan dengan package name Coach
+    compileSdk flutter.compileSdkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility JavaVersion.VERSION_17
+        targetCompatibility JavaVersion.VERSION_17
     }
 
-    // Cara penulisan jvmTarget yang sah dan disukai AGP terbaru, ditaruh di dalam tasks
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-        kotlinOptions {
-            jvmTarget = "17"
-        }
+    kotlinOptions {
+        jvmTarget = '17'
     }
 
     defaultConfig {
-        applicationId = "com.heruwngchn.addhmescrn" // Sesuaikan juga di sini Coach
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        applicationId "com.heruwngchn.addhmescrn" // Sesuaikan juga di sini
+        minSdk flutter.minSdkVersion
+        targetSdk flutter.targetSdkVersion
+        versionCode flutter.versionCode
+        versionName flutter.versionName
     }
 
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig signingConfigs.debug
         }
     }
 }
 
 flutter {
-    source = "../.."
+    source "../.."
 }
